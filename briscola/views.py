982f865a08be106b268596e0d9bf52afdecc75af
Matt Lappin <mlappin3@gatech.edu>
2022-01-09 15:52:03 -0800

    create/ endpoint uses JsonResponse now

--- briscola/views.py
+++ briscola/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def create(request):
-    return HttpResponse("{test: 'data'}")
+    test_data = {'test' : 'data'}
+    return JsonResponse(test_data)
